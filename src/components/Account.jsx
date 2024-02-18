688e43edae0a519dcb0e38b69c88227735e36125
Brian <mulindijrn@gmail.com>
2024-02-19 01:05:42 +0300

    Create an account component that includes the account details that is login and sign up and the profile

---
+++ src/components/Account.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SignUp from "./SignUp"
+import Login from './Login'
+
+function Account() {
+  return (
+    <>
+      <SignUp />
+      <Login />
+    </>
+  )
+}
+
+export default Account
