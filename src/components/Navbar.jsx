758ce6138fddffe9a6c0ee94dd8d39bcf64924cc
Brian <mulindijrn@gmail.com>
2024-02-19 00:56:46 +0300

    Implement basic Navbar component with toggle functionality for categories and account visibility

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,23 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
+function Navbar() {
+  const [showCategories, setShowCategories] = useState(false);
+  const [showAccount, setShowAccount] = useState(false);
 
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
+
+  const toggleAccount = () => {
+    setShowAccount(!showAccount);
+  };
+
+  return (
+    <nav>
+      
+    </nav>
+  );
+}
 
 export default Navbar;
