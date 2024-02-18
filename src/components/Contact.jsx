5bc84d2cd5cdc1d629750796fb62d1ab110e88fc
Markswell <osoromarkswell80@gmail.com>
2024-02-18 22:45:10 +0300

    Embed Google Map Location

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,91 +1,108 @@
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
 import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons'; 
 import { library } from '@fortawesome/fontawesome-svg-core'; 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import emailjs from 'emailjs-com';
 import '../contact.css'; 
 
 library.add(faMapMarkerAlt, faPhone, faEnvelope, faFacebook, faTwitter, faInstagram);
 
 const Contact = () => {
+    const [showModal, setShowModal] = useState(false);
+    const [modalMessage, setModalMessage] = useState('');
+
     useEffect(() => {
         emailjs.init('HxDcaC4Cx86JLPUPx'); 
-      }, []);
+    }, []);
     
-      const handleSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
-    
-        emailjs.sendForm('service_i12pgxp', 'template_ls365cr', e.target, 'YOUR_USER_ID')
-          .then((result) => {
-            console.log('Email sent successfully:', result.text);
-            alert('Email sent successfully!');
-          }, (error) => {
-            console.error('Error sending email:', error);
-            alert('Failed to send email. Please try again later.');
-          });
-            e.target.reset();
-      };
-    
 
-  return (
-    <div className="contact mt-4">
-      <Container>
-        <Row>
-          <Col md={6}>
-            <h2 className="text-center text-navy">Contact Information</h2>
-            <ul className="list-unstyled text-center">
-              <li><FontAwesomeIcon icon={faMapMarkerAlt} /> 123 Street, Nairobi, Kenya</li>
-              <li><FontAwesomeIcon icon={faPhone} /> +254 798 840 098</li>
-              <li><FontAwesomeIcon icon={faEnvelope} /> info@electroportsandpins.com</li>
-            </ul>
-            <div className="social-links text-center mt-4">
-              <a href="https://www.facebook.com" className="social-link"><FontAwesomeIcon icon={faFacebook} size="lg" /></a>
-              <a href="https://www.twitter.com" className="social-link"><FontAwesomeIcon icon={faTwitter} size="lg" /></a>
-              <a href="https://www.instagram.com" className="social-link"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
-            </div>
-            <Row className="justify-content-center mt-4">
-              <h2 className="text-center text-navy">Location</h2>
-              {/* Embed Google Map using Google Maps Embed API */}
-              <iframe 
-                title="Google Map"
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.79061004006!2d36.7846067!3d-1.3004862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1a6bf7445dc1%3A0x940b62a3c8efde4c!2sMoringa%20School!5e0!3m2!1sen!2ske!4v1708249831100!5m2!1sen!2ske" 
-                width="300" 
-                height="300" 
-                style={{ border: 0 }}
-                allowFullScreen=""
-                loading="lazy">
-              </iframe>
-            </Row>
-          </Col>
-          <Col md={6}>
-            <h2 className="text-center text-navy">Contact Form</h2>
-            <Form className="contact-form mt-3" style={{ width: '80%', margin: '0 auto' }} onSubmit={handleSubmit}>
-              <Form.Group controlId="formName">
-                <Form.Label className="navy-label mt-2">Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" />
-              </Form.Group>
+        emailjs.sendForm('service_i12pgxp', 'template_ls365cr', e.target, 'HxDcaC4Cx86JLPUPx')
+            .then((result) => {
+                console.log('Email sent successfully:', result.text);
+                setModalMessage('Email sent successfully!');
+                setShowModal(true);
+            })
+            .catch((error) => {
+                console.error('Error sending email:', error);
+                setModalMessage('Failed to send email. Please try again later.');
+                setShowModal(true);
+            });
+        e.target.reset();
+    };
 
-              <Form.Group controlId="formEmail">
-                <Form.Label className="navy-label mt-2">Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
-              </Form.Group>
+    return (
+        <div className="contact mt-4">
+            <Container>
+                <Row>
+                    <Col md={5} style={{ width: '35%', margin: '0 auto' }}>
+                        <h2 className="text-center text-navy">Contact Information</h2>
+                        <ul className="list-unstyled text-left">
+                            <li><FontAwesomeIcon icon={faMapMarkerAlt} /> 123 Street, Nairobi, Kenya</li>
+                            <li><FontAwesomeIcon icon={faPhone} /> +254 798 840 098</li>
+                            <li><FontAwesomeIcon icon={faEnvelope} /> info@electroportsandpins.com</li>
+                        </ul>
+                        <div className="social-links text-center mt-4">
+                            <a href="https://www.facebook.com" className="social-link"><FontAwesomeIcon icon={faFacebook} size="lg" /></a>
+                            <a href="https://www.twitter.com" className="social-link"><FontAwesomeIcon icon={faTwitter} size="lg" /></a>
+                            <a href="https://www.instagram.com" className="social-link"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                        </div>
+                        <Row className="justify-content-center mt-4">
+                            <h2 className="text-center text-navy">Location</h2>
+                            {/* Embed Google Map using Google Maps Embed API */}
+                            <iframe 
+                                title="Google Map"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.79061004006!2d36.7846067!3d-1.3004862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1a6bf7445dc1%3A0x940b62a3c8efde4c!2sMoringa%20School!5e0!3m2!1sen!2ske!4v1708249831100!5m2!1sen!2ske" 
+                                width="80%" 
+                                height="300" 
+                                style={{ border: 0 }}
+                                allowFullScreen=""
+                                loading="lazy">
+                            </iframe>
+                        </Row>
+                    </Col>
+                    <Col md={6}>
+                        <h2 className="text-center text-navy">Contact Form</h2>
+                        <Form className="contact-form mt-3" style={{ width: '80%', margin: '0 auto' }} onSubmit={handleSubmit}>
+                            <Form.Group controlId="formName">
+                                <Form.Label className="navy-label mt-2">Name</Form.Label>
+                                <Form.Control type="text" placeholder="Enter your name" />
+                            </Form.Group>
 
-              <Form.Group controlId="formMessage">
-                <Form.Label className="navy-label mt-2">Message</Form.Label>
-                <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
-              </Form.Group>
+                            <Form.Group controlId="formEmail">
+                                <Form.Label className="navy-label mt-2">Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter your email" />
+                            </Form.Group>
 
-              <Button variant="primary" type="submit" className="custom-button mx-auto d-block mt-3">
-                Submit
-              </Button>
-            </Form>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+                            <Form.Group controlId="formMessage">
+                                <Form.Label className="navy-label mt-2">Message</Form.Label>
+                                <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit" className="custom-button mx-auto d-block mt-3">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+            {/* Bootstrap Modal for displaying alerts */}
+            <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+                <Modal.Header closeButton>
+                    <Modal.Title>Alert</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{modalMessage}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="primary" onClick={() => setShowModal(false)}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
 }
 
 export default Contact;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
-import SignUp from './components/SignUp.jsx';
-// import Login from './components/Login.jsx';
-// import About from './components/About.jsx';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
