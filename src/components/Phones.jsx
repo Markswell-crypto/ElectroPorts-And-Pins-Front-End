77e80ebb6fa5d8f9b1037c234bcfb36f72c278e5
Mercy <mercy.mwongeli@student.moringaschool.com>
2024-02-20 18:51:40 +0300

    done changes to phones.jsx

--- src/components/Phones.jsx
+++ src/components/Phones.jsx
@@ -1,9 +1,9 @@
+// Phones.jsx
 import React, { useEffect, useState } from 'react';
 import { Col, Row, Button } from 'react-bootstrap';
 
-function Phones() {
+function Phones({ addToCart }) {
   const [phones, setPhones] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch('https://electroports-db.onrender.com/phones')
@@ -11,12 +11,6 @@ function Phones() {
       .then(data => setPhones(data.phones))
       .catch(error => console.error('Error fetching phones:', error));
   }, []);
-
-  const handleOrder = (phone) => {
-    // Adding the selected phone to the cart
-    setCart([...cart, phone]);
-    console.log(`Added ${phone.name} to cart`);
-  };
 
   return (
     <div className="container">
@@ -28,7 +22,7 @@ function Phones() {
               <img src={phone.image_url} alt={phone.name} className="img-fluid mb-3" style={{ maxHeight: '200px', objectFit: 'cover' }} />
               <div className="mb-2">Name: {phone.name}</div>
               <div className="mb-2">Price: {phone.price}</div>
-              <Button onClick={() => handleOrder(phone)}>Add to Cart</Button>
+              <Button onClick={() => addToCart(phone)}>Add to Cart</Button>
               <div>Status: {phone.status}</div>
             </div>
           </Col>
