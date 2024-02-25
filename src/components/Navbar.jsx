9122b03e748c47982ce84bf01fd2d6290f51feba
Brian <mulindijrn@gmail.com>
2024-02-25 21:29:53 +0300

    Add Signing Up link in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,10 @@ function Navbar() {
               {isLoggedIn ? (
                 <button onClick={handleLogout}>Logout</button>
               ) : (
-                <li><Link to="/login">Login</Link></li>
+                <>
+                  <li><Link to="/login">LogIn</Link></li>
+                  <li><Link to="/signup">SignUp</Link></li>
+                </>
               )}
             </ul>
           )}
